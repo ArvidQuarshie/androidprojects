1f2110dd26cb708503c188e52421aa37e42fdbdd
ArvidQuarshie <arvidquarshie@gmail.com>
2016-05-23 23:30:47 +0300

    android projects

---
+++ BrowserApp/app/src/main/java/com/example/user/browserapp/Display.java
@@ -0,0 +1,17 @@
+package com.example.user.browserapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by user on 5/22/16.
+ */
+public class Display extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.display);
+    }
+
+
+}
